e8da48ccbe331f720972a3a71b293a5dafb3d977
bilel abbassi <bilel.ab@esprit.tn>
2023-09-03 14:36:02 +0100

    change

--- src/components/HomepageFeatures/index.tsx
+++ src/components/HomepageFeatures/index.tsx
@@ -10,7 +10,7 @@ type FeatureItem = {
 
 const FeatureList: FeatureItem[] = [
   {
-    title: 'Easy to Use',
+    title: 'dsnfdnf,df',
     Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
     description: (
       <>
