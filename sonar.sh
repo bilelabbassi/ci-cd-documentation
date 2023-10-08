b1409acf175fc172c0a06df925401f129728342e
bilel abbassi <bilel.ab@esprit.tn>
2023-10-08 16:57:12 +0100

    sonar

--- sonar.sh
+++ sonar.sh
@@ -1,6 +1,8 @@
 sonar-scanner \
-   -Dsonar.projectKey=documentationf \
+   -Dsonar.projectKey=documentationf       \
    -Dsonar.sources=./src \
    -Dsonar.host.url=http://localhost:9000 \
    -Dsonar.login=admin \
    -Dsonar.password=sonar
+
+   docker push bilelabbass/repos_fromation:production
